76db1b305e74e90d1c2c4acc278ae9f31e3b7399
Trish Whetzel <plwhetzel@gmail.com>
2020-02-13 20:22:42 +0000

    add all forms to one page

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import BasicForm from './BasicForm';
-import SyncValidationForm from './SyncValidationForm';
-import MaterialSyncForm from './MaterialSyncForm';
 
 ReactDOM.render(
-    // <App />, document.getElementById('root'),
-    // <BasicForm />, document.getElementById('basicForm')
-    // <SyncValidationForm />, document.getElementById('syncForm')
-    <MaterialSyncForm />, document.getElementById('matSyncForm')
+    <App />, document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
